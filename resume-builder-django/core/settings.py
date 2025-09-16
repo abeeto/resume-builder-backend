285e9673c476690c7fbce3f8f0a76b871d73cd5a
abeeto <59943893+abeeto@users.noreply.github.com>
2025-09-16 18:47:56 +0530

    build: config cors

--- resume-builder-django/core/settings.py
+++ resume-builder-django/core/settings.py
@@ -56,11 +56,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'drf_spectacular',
     'resume_builder',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
